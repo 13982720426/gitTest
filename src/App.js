3627f85ab4e7cc58bbe7c23ecde1297e36114004
jackhoo_98 <jackhoo_98@foxmail.com>
2024-08-25 22:33:29 +0800

    feat: 添加 commit-msg

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from 'react';
+// import App2 from "./App2.tsx";
 import App2 from './App2.tsx';
 
 function App() {
@@ -8,9 +9,24 @@ function App() {
   let a = 1;
   const b = 3;
   console.log(a + b);
+
+  const fn = (c) => {
+    const arr = [1, 2, 3];
+    arr.forEach((item) => {
+      console.log(item);
+    });
+    console.log(a + b, c);
+  };
+  fn(3);
+  // const fn=(c)=>{
+  //   console.log(a+b,c);
+  // }
+  // fn(3)
+
   return (
     <div className='App'>
-      <div>App</div>
+      <div> App</div>
+      <div>123</div>
       <App2></App2>
     </div>
   );
--- src/App2.tsx
+++ src/App2.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
+// const e = 'World';
+// const f = '!';
 const App2 = () => {
   // const a:any=1;
   const a = 1;
+  // const b = 2;
+  // const c = 3;
+  // var b=2
+  // let c=3
+  const b = 2;
+  const c = 3;
+  // var b:any=2
+  console.log(a + b, c);
   return (
     <div>
       App2
